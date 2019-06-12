88a3bc4bc7f63400ca5b84758a9d135a6b840e18
Jody Vandenschrick <jody.vandenschrick@cultureamp.com>
2019-06-12 16:38:49 +0800

    implementing the command move

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -1,20 +1,45 @@
 defmodule ToyRobot do
+  defstruct [north: 0, east: 0, facing: :north]
+  alias ToyRobot, as: Robot
   @moduledoc """
   Documentation for ToyRobot.
   """
 
   @doc """
-  Moves one space forward.
+  Moves the robot forward on space in the direciton it is facing
 
   ## Examples
 
-      iex> ToyRobot.move(%{position: 0})
-      %{position: 1}
-
-      iex> %{position: 0} |> ToyRobot.move |> ToyRobot.move
-      %{position: 2}
+      iex> alias ToyRobot, as: Robot
+      ToyRobot
+      iex> robot = %Robot{north: 0, facing: :north}
+      %Robot{north: 0, facing: :north}
+      iex> robot |> Robot.move
+      %Robot{north: 1}
   """
-  def move(%{position: position}) do
-    %{position: position + 1}
+
+  def move(%Robot{facing: facing} = robot) do
+    case facing do
+      :north -> robot |> move_north
+      :south -> robot |> move_south
+      :west -> robot |> move_west
+      :east -> robot |> move_east
+    end
+  end
+
+  defp move_east(robot) do
+    %Robot{east: robot.east + 1}
+  end
+
+  defp move_west(robot) do
+    %Robot{east: robot.east - 1}
+  end
+
+  defp move_north(robot) do
+    %Robot{north: robot.north + 1}
+  end
+
+  defp move_south(robot) do
+    %Robot{north: robot.north - 1}
   end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -1,8 +1,49 @@
 defmodule ToyRobotTest do
+  alias ToyRobot, as: Robot
   use ExUnit.Case
   doctest ToyRobot
 
-  test "more complex stuff goes here" do
+  describe "when the robot is facing north" do
+    setup do
+      {:ok, %{robot: %Robot{north: 0, facing: :north}}}
+    end
 
+    test "it moves one space north", %{robot: robot} do
+      robot = robot |> Robot.move
+      assert robot.north == 1
+    end
+  end
+
+  describe "when the robot is facing east" do
+    setup do
+      {:ok, %{robot: %Robot{east: 0, facing: :east}}}
+    end
+
+    test "it moves one space east", %{robot: robot} do
+      robot = robot |> Robot.move
+      assert robot.east == 1
+    end
+  end
+
+  describe "when the robot is facing south" do
+    setup do
+      {:ok, %{robot: %Robot{north: 0, facing: :south}}}
+    end
+
+    test "it moves one space south", %{robot: robot} do
+      robot = robot |> Robot.move
+      assert robot.north == -1
+    end
+  end
+
+  describe "when the robot is facing west" do
+    setup do
+      {:ok, %{robot: %Robot{east: 0, facing: :west}}}
+    end
+
+    test "it moves one space north", %{robot: robot} do
+      robot = robot |> Robot.move
+      assert robot.east == -1
+    end
   end
 end
